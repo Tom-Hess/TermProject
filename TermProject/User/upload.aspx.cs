ddbfd004dabedec2102f3e461821978dacc7527d
Tom1759 <tuf34452@temple.edu>
2017-04-21 10:18:56 -0400

    don't allow music, video, or images. add a couple file types

--- TermProject/User/upload.aspx.cs
+++ TermProject/User/upload.aspx.cs
@@ -62,97 +62,119 @@ namespace TermProject.User
 
                 fileExtension = fileExtension.ToLower();
 
-                string imagePath;
+                string imagePath = "";
+                bool valid = true;
 
-                switch(fileExtension)
+                switch (fileExtension)
                 {
                     case ".pdf":
                         imagePath = "~/Images/pdf.jpg";
                         break;
+                        //images cannot be uploaded
+                    case ".png":
                     case ".jpg":
-                        imagePath = "~/Images/jpg.png";
-                        break;
                     case ".jpeg":
-                        imagePath = "~/Images/jpeg.jpg";
+                        valid = false;
                         break;
                     case ".docx":
                     case ".doc":
                         imagePath = "~/Images/word.jpg";
                         break;
-                    case ".gif":
-                        imagePath = "~/Images/gif.jpg";
-                        break;
-                    case ".png":
-                        imagePath = "~/Images/png.jpg";
-                        break;
                     case ".xlsx":
                         imagePath = "~/Images/xslx.png";
                         break;
+                    case ".txt":
+                        imagePath = "~/Images/txt.jpg";
+                        break;
+                    case ".aspx":
+                        imagePath = "~/Images/aspx.jpg";
+                        break;
                     case ".pptx":
+                    case ".ppt":
                         imagePath = "~/Images/pptx.png";
                         break;
                     case ".exe":
                         imagePath = "~/Images/exe.jpg";
                         break;
+                        //music files cannot be uploaded
                     case ".mp3":
-                        imagePath = "~/Images/mp3.jpg";
+                        valid = false;
                         break;
                     case ".zip":
+                    case ".7z":
                         imagePath = "~/Images/zip.jpg";
+                        break;
+                        //video formats cannot be uploaded
+                    case ".mp4":
+                    case ".gif":
+                    case ".wmv":
+                    case ".avi":
+                    case ".flv":
+                    case ".wav":
+                    case ".wma":
+                        valid = false;
                         break;
                     default:
                         imagePath = "~/Images/unknown.png";
                         break;
                 }
                 
-                lblMsg.Text = fileData.ToString() + fileTitle;
 
-                Part2WS.FileData newFileData = new Part2WS.FileData();
-                newFileData.AccountID = Convert.ToInt32(Session["AccountID"]);
-                newFileData.Email = Session["email"].ToString();
-                newFileData.Timestamp = DateTime.Now;
-                newFileData.Title = fileTitle;
-                newFileData.Type = fileType;
-                newFileData.Length = fileLength;
-                newFileData.ImagePath = imagePath;
-                newFileData.Extension = fileExtension;
-                
-                myAccount = myUpload.GetAccountInfo(Session["email"].ToString(), 
-                    Convert.ToInt32(Session["verification"]));
-                FileCloud cloud = (FileCloud)Session["cloud"];
-
-                FileData myFile = new FileData();
-
-                for (int i = 0; i < cloud.Files.Count; i++)
+                if(valid)
                 {
-                    myFile = (FileData)cloud.Files[i];
+                    lblMsg.Text = fileData.ToString() + fileTitle;
 
-                    if (myFile.Title == fileTitle)
+                    Part2WS.FileData newFileData = new Part2WS.FileData();
+                    newFileData.AccountID = Convert.ToInt32(Session["AccountID"]);
+                    newFileData.Email = Session["email"].ToString();
+                    newFileData.Timestamp = DateTime.Now;
+                    newFileData.Title = fileTitle;
+                    newFileData.Type = fileType;
+                    newFileData.Length = fileLength;
+                    newFileData.ImagePath = imagePath;
+                    newFileData.Extension = fileExtension;
+
+                    myAccount = myUpload.GetAccountInfo(Session["email"].ToString(),
+                        Convert.ToInt32(Session["verification"]));
+                    FileCloud cloud = (FileCloud)Session["cloud"];
+
+                    FileData myFile = new FileData();
+
+                    for (int i = 0; i < cloud.Files.Count; i++)
                     {
-                        //add updating existing file
-                        lblMsg.Text = "File title already in use!";
-                        break;
+                        myFile = (FileData)cloud.Files[i];
+
+                        if (myFile.Title == fileTitle)
+                        {
+                            //add updating existing file
+                            lblMsg.Text = "File title already in use!";
+                            break;
+                        }
+
                     }
 
-                }
+                    if (fileLength > (myAccount.StorageSpace - myAccount.StorageUsed))
+                    {//If file size is bigger than the user's current balance
+                        lblMsg.Text = "You don't have enough storage in your cloud to store this file. ";
+                    }
+                    else
+                    {
+                        int ID = CloudWS.addDownloadData(Convert.ToInt32(Session["AccountID"]),
+                            fileData, Convert.ToInt32(Session["verification"]));
+                        myUpload.updateStorageUsed(Session["email"].ToString(), fileLength,
+                            Convert.ToInt32(Session["verification"]));
+                        lblMsg.ForeColor = System.Drawing.Color.Green;
+                        lblMsg.Text = "Successfully uploaded " + fileTitle;
+                        newFileData.FileID = ID;
 
-                if (fileLength > (myAccount.StorageSpace - myAccount.StorageUsed))
-                {//If file size is bigger than the user's current balance
-                    lblMsg.Text = "You don't have enough storage in your cloud to store this file. ";
-                }
-                else
+                        cloud = (FileCloud)Session["cloud"];
+                        cloud.Files.Add(newFileData);
+                    }
+                }else
                 {
-                    int ID = CloudWS.addDownloadData(Convert.ToInt32(Session["AccountID"]), 
-                        fileData, Convert.ToInt32(Session["verification"]));
-                    myUpload.updateStorageUsed(Session["email"].ToString(), fileLength,
-                        Convert.ToInt32(Session["verification"]));
-                    lblMsg.ForeColor = System.Drawing.Color.Green;
-                    lblMsg.Text = "Successfully uploaded " + fileTitle;
-                    newFileData.FileID = ID;
-
-                    cloud = (FileCloud)Session["cloud"];
-                    cloud.Files.Add(newFileData);
+                    lblMsg.Text = "Invalid file type. Images, videos and music files cannot be uploaded.";
                 }
+                
             }
         }
                 
